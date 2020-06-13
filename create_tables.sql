b40054aa765eae1e2653e0427aefa2db9d4523aa
Kathryn Panger <kpanger88@gmail.com>
2020-06-12 20:56:12 -0700

    building tables

---
+++ create_tables.sql
@@ -0,0 +1,38 @@
+--one to many
+CREATE TABLE employees(
+	emp_no int PRIMARY KEY,
+	emp_title_id VARCHAR,
+	birth_date DATE,
+	first_namke VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_date DATE,
+	FOREIGN KEY (emp_title_id)REFERENCES titles(title_id))
+
+--one to one
+CREATE TABLE titles(
+	title_id VARCHAR PRIMARY KEY,
+	title VARCHAR)
+	
+
+	
+--one to many	
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY,
+	salary 	INT)
+
+--many to many
+CREATE TABLE dept_emp(
+	emp_no INT PRIMARY KEY,
+	dept_no VARCHAR)
+
+--one to many
+CREATE TABLE dept_manager(
+	emp_no INT PARIMARY KEY,
+	dept_no VARCHAR)
+
+--one to one
+CREATE TABLE departments(
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR)
+	
